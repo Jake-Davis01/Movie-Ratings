4b5c617c215ba2b7c9d0bea26c3067642db05677
Obi Ojji <obi.ojji@lafosseacademy.com>
2026-04-09 11:07:19 +0100

    html, CSS changes and APIs for movies

--- FrontEnd/Scripts/app.js
+++ FrontEnd/Scripts/app.js
@@ -1,55 +1,71 @@
 // get data from omdb api
 //import "dotenv/config"
 //const apiKey = process.env.omdb_apikey
-const apiKey = "b70d46ec"
+const apiKey = "b70d46ec";
 
 // need to find the rotten tomato score first
-function mapForDB(data) { 
-    const rtScore = data.Ratings?.find(r => r.Source === "Rotten Tomatoes")
+function mapForDB(data) {
+  const rtScore = data.Ratings?.find((r) => r.Source === "Rotten Tomatoes");
 
-    return {
-        movie_name: data.Title || "Unknown",
-        year: data.Year || "0",
-        rating: 0,
-        imdb: data.imdbRating || "0.0",
-        rotten: rtScore ? rtScore.Value : "N/A",
-        image: data.Poster,
-        user_id: 999 //need to add the getMe for the session and add this as an or
-
-    }
+  return {
+    movie_name: data.Title || "Unknown",
+    year: data.Year || "0",
+    rating: 0,
+    imdb: data.imdbRating || "0.0",
+    rotten: rtScore ? rtScore.Value : "N/A",
+    image: data.Poster,
+    user_id: 999, //need to add the getMe for the session and add this as an or
+  };
 }
 
-async function getMovieDetails(title) {
-    try {
-        // make url
-        const movieUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`
+async function getMovieDetails(title, page = 1, year = "") {
+  try {
+    // make url
+    const movieUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${title}&page=${page}&type=movie&y=${year}`;
 
-        // request and capture
-        const movieResponse = await fetch(movieUrl)
-        const movieData = await movieResponse.json()
+    // request and capture
+    const movieResponse = await fetch(movieUrl);
+    const movieData = await movieResponse.json();
 
-        //handle response error
-        if (movieData.Response === "False") {
-            console.log("Sorry movie not found")
-            return "Movie not found ❌"
-        }
-
-        
-
-        console.log("Movie data found! 🎬", movieData)
-        //return mapForDB(movieData)
-        return movieData
-
-    } catch (err) {
-<<<<<<< HEAD:FrontEnd/app.js
-        console.error(`Error: ${err}`)
+    //handle response error
+    if (movieData.Response === "False") {
+      console.log("Sorry movie not found");
+      return "Movie not found ❌";
     }
-    
-=======
-        console.error(`Error: {err}`)
-    }   
->>>>>>> main:FrontEnd/Scripts/app.js
+
+    console.log("Movie data found! 🎬", movieData);
+    //return mapForDB(movieData)
+    return movieData;
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
 }
+
+async function selectMovie(imdbID) {
+  // use imdbID from attribute on selection, eventlistener on click feeds it to the function
+  try {
+    const selectedMovieUrl = `http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`;
+
+    const selectedMovie = await fetch(selectedMovieUrl);
+    const movieInfo = await selectedMovie.json();
+
+    // handle error
+    if (movieInfo.Response === "False") {
+      console.log("Sorry movie not found");
+      return "Movie not found ❌";
+    }
+
+    console.log("Movie data found! 🎬", movieInfo);
+    // will need to change to mapForDB
+    return movieInfo;
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+}
+
+/*
+const testMovie = await selectMovie("tt0372784");
+console.log(testMovie);
 
 /*
 export{
